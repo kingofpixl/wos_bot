9b52c241ca1622b22b067bd04ff3f04a5582e6d7
Wesley Waldern <wesley.waldern@uleth.ca>
2025-06-15 15:09:18 -0600

    updated dictionary to get functions

--- steam_api_calls.py
+++ steam_api_calls.py
@@ -16,14 +16,14 @@ def get_game(user, game_name: str):
     game = steam.apps.search_games(term=game_name, country='CA')
     return f'hello {user.mention} {game['apps'][0]['name']} is {game['apps'][0]['price']}'
 
-
+# your id: 76561198061247902
 def recently_played_games(user_id):
     recent_games = steam.users.get_user_recently_played_games(user_id)['games']
     list_of_recent_games = []
     games_played = f'here are the users recently played games: '
 
     for game in recent_games:
-        list_of_recent_games.append(f'{game['name']} for {round(float(int(game['playtime_2weeks'])/60), 2)} hours')
+        list_of_recent_games.append(f"{game.get('name')} for {round(float(int(game.get('playtime_2weeks')) / 60), 2)} hours")
 
     empty_string = ', '.join(list_of_recent_games)
 
